6640c9df424e2063b448016dad47e6cf48076c1e
Jesse Choe <67716130+jessechoe10@users.noreply.github.com>
2021-04-25 21:08:59 -0400

    Update twoarrayfunctions.cpp

--- CF/Sorting and Searching/1600/twoarrayfunctions.cpp
+++ CF/Sorting and Searching/1600/twoarrayfunctions.cpp
@@ -1,26 +1,26 @@
-    #include <bits/stdc++.h>
-     
-    using namespace std;
-    using ll = long long;
-     
-    const ll MOD = 998244353;
-     
-    int main(){
-        int n; cin >> n;
-        vector<ll> a(n);
-        for(int i=0; i<n; i++){
-            cin >> a[i];
-            a[i] = (a[i] * (i+1) * (n-i));
-        }
-        vector<ll> b(n);
-        for(int i=0; i<n; i++){
-            cin >> b[i];
-        }
-        sort(a.begin(), a.end(), greater<ll>());
-        sort(b.begin(), b.end());
-        ll ans = 0;
-        for(int i=0; i<n; i++){
-            ans = ((ans + (a[i]%MOD) * b[i]) % MOD) % MOD;
-        }
-        cout << ans << endl;
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+const ll MOD = 998244353;
+
+int main(){
+    int n; cin >> n;
+    vector<ll> a(n);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+        a[i] = (a[i] * (i+1) * (n-i));
     }
+    vector<ll> b(n);
+    for(int i=0; i<n; i++){
+        cin >> b[i];
+    }
+    sort(a.begin(), a.end(), greater<ll>());
+    sort(b.begin(), b.end());
+    ll ans = 0;
+    for(int i=0; i<n; i++){
+        ans = ((ans + (a[i]%MOD) * b[i]) % MOD) % MOD;
+    }
+    cout << ans << endl;
+}
