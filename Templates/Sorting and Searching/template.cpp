a45cd44b65790ee64942c2ef2f065a066891c7e3
Jesse Choe <67716130+jessechoe10@users.noreply.github.com>
2021-05-03 13:49:19 -0400

    Create template.cpp

---
+++ Templates/Sorting and Searching/template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vpi = vector<pi>;
+
+using msi = multiset<int>;
+using si = set<int>;
+
+using vl = vector<ll>;
+using pl = pair<ll, ll>;
+using vpl = vector<pl>;
+
+using msl = multiset<ll>;
+using sl = set<ll>;
+
+#define print(x) cout << x << endl
+#define dbg(x) for(auto& a: x) cout << a << " "
+#define all(x) begin(x), end(x)
+#define sor(x) sort(all(x))
+#define rsor(x) sort(all(X), greater<ll>())
+#define ub(a, x) upper_bound(all(a), x) - begin(a)
+#define lb(a, x) lower_bound(all(a), x) - begin(a)
+#define FASTIO cin.tie(0)->sync_with_stdio(0)
+
+int main(){
+  	FASTIO;
+}
