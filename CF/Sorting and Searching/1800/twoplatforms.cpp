0fa1b0723cd216672f77b76d9ffd0d4cfa92cae7
Jesse Choe <67716130+jessechoe10@users.noreply.github.com>
2021-05-09 15:42:12 -0400

    Update twoplatforms.cpp

--- CF/Sorting and Searching/1800/twoplatforms.cpp
+++ CF/Sorting and Searching/1800/twoplatforms.cpp
@@ -1,3 +1,19 @@
+// Problem Link: https://codeforces.com/problemset/problem/1409/E
+// Official Editorial: https://codeforces.com/blog/entry/82284
+
+/*
+	YoRepi7's analysis:
+
+	We can ignore the y-coordinates in this problem since it suffices to put the platforms at -10^9. Thus, we
+	should only consider the x-coordinates in this problem. Obviously, the platforms must be between at least
+	one point (with respect to the x-coordinate of that point). Thus, it suffices to find the length of the
+	maximum platform continaing a single x-coordinate. However, the problem indicates that there could be up
+	to two platforms; thus, it suffices to store the maximum platform after each x-coordinate and add it to
+	the total platform length at a given index i. We can compute the maximum length of the containing x_i using
+	binary search. The total runtime of this algorithm is O(n log n) since we must sort the x-coordinates prior
+	to applying our algorithm above. 
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
