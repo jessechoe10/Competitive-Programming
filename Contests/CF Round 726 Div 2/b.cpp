f8b6b1923705f246e8dee3f39ec1e307e32303d1
Jesse Choe <67716130+jessechoe10@users.noreply.github.com>
2021-06-23 21:11:51 -0400

    Create b.cpp

---
+++ Contests/CF Round 726 Div 2/b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+void solve(){
+	ll n, m, i, j; cin >> n >> m >> i >> j;
+	cout << 1 << " " << 1 << " " << n << " " << m << endl;
+}
+
+int main(){
+	int t; cin >> t;
+	for(int i = 0; i < t; i++){
+		solve();
+	}
+}
